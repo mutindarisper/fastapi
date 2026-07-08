1b92dff08f69dcdb27fe1682fddf786fe6199e1b
mutindarisper <mutindarisper@gmail.com>
2026-07-08 17:04:30 +0300

    query params

--- books.py
+++ books.py
@@ -21,3 +21,26 @@ def read_book(book_title: str):
     for book in BOOKS:
         if book["title"].casefold() == book_title.casefold():
             return book
+        
+
+  
+#query parameters
+
+@app.get("/books/")
+def read_category_by_query(category:str):
+    books_to_return = []
+    for book in BOOKS:
+        if book["category"].casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+#path and query parameters combined
+@app.get("/books/{book_author}/")
+def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book["author"].casefold() == book_author.casefold() and book["category"].casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+    
