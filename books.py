8e8d8e2aacf139a5d07feca068e2b10874f7d8d4
mutindarisper <mutindarisper@gmail.com>
2026-07-08 16:19:55 +0300

    get a list of all books

--- books.py
+++ books.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "math"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "science"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"}
+]
 
 @app.get("/books")
-def first_api():
-    return {"message": "building my first API!"}
+def read_all_books():
+    return BOOKS
