acce7340c7b2b5e61675941d0e75649ba0406b68
mutindarisper <mutindarisper@gmail.com>
2026-07-11 00:04:41 +0300

    add exceptions and status codes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 from sqlalchemy.orm import Session
 from typing import Annotated
 import models
@@ -20,6 +20,14 @@ def get_db():
 
 db_dependency =  Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency): #dependency injection - this depends on the get_db function to get the db session
-    return db.query(Todos).all()
+    return db.query(Todos).all() #query the db of all the Todos
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK) #get a specific todo by id
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first() #for performance, we use first() instead of all() to get the first result that matches the filter
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found") #if the todo is not found, return a 404 error
