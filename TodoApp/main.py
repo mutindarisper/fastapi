5f016d7cb3ac3ef360d77cc17cd72f03b4e60052
mutindarisper <mutindarisper@gmail.com>
2026-07-11 00:40:49 +0300

    update a record and save to db

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -49,3 +49,18 @@ async def create_todo(todo_request: TodoRequest, db: db_dependency):
     db.add(todo_model) #about to add the new todo to the db session
     db.commit() #commit the changes to the db
   
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT) #update a todo by id
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+   
+   todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+   if todo_model is None:
+       raise HTTPException(status_code = 404, detail="Todo not found")
+   todo_model.title = todo_request.title
+   todo_model.description = todo_request.description
+   todo_model.priority = todo_request.priority
+   todo_model.complete = todo_request.complete
+
+   db.add(todo_model)
+   db.commit()
+
